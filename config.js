efd59c01d5682dcee800a185b1590069e881c6d3
tecnicoweb85 <36242918+tecnicoweb85@users.noreply.github.com>
2018-10-11 22:53:53 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'profesor10demates',
+		wp_ws_url : 'https://www.profesor10demates.com/wp-appkit-api/profesor10demates',
+		wp_url : 'https://www.profesor10demates.com',
+		theme : 'q-android',
+		version : '1',
+		app_type : 'phonegap-build',
+		app_title : 'Profesor10demates',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'kUe^2qj[Xf)d`4W)9^b$xQ;zt~s`d1gh,us C+vJHMKe2MYxMyOBS[@b6_aBG]s=',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
